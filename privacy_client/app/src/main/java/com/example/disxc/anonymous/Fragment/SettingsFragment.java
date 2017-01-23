8340f60994ed24a146a4e8569a4a20e0e92e033a
nhne <disoxc21@gmail.com>
2017-01-23 17:59:40 +0900

    google+ 제거, gradle 수정

--- privacy_client/app/src/main/java/com/example/disxc/anonymous/Fragment/SettingsFragment.java
+++ privacy_client/app/src/main/java/com/example/disxc/anonymous/Fragment/SettingsFragment.java
@@ -72,7 +72,7 @@ public class SettingsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         //Find the +1 button
-        mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
+        //mPlusOneButton = (PlusOneButton) view.findViewById(R.id.plus_one_button);
 
         return view;
     }
@@ -82,7 +82,7 @@ public class SettingsFragment extends Fragment {
         super.onResume();
 
         // Refresh the state of the +1 button each time the activity receives focus.
-        mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
+        //mPlusOneButton.initialize(PLUS_ONE_URL, PLUS_ONE_REQUEST_CODE);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
