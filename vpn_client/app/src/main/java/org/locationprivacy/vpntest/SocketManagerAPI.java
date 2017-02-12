c429975ed40d9494a0198e0c0adbaec701b67fb0
hw5773 <hw5773@gmail.com>
2017-02-12 17:27:14 +0900

    Change the interface

--- vpn_client/app/src/main/java/org/locationprivacy/vpntest/SocketManagerAPI.java
+++ vpn_client/app/src/main/java/org/locationprivacy/vpntest/SocketManagerAPI.java
@@ -4,7 +4,7 @@ package org.locationprivacy.vpntest;
  * Created by HWY on 2017-02-06.
  */
 
-public interface SocketManager {
+public interface SocketManagerAPI {
     // Adding the socket in the SocketManager
     void addSocket(boolean isTCP, IP_Header ipHdr, TCP_Header tcpHdr);
 
@@ -12,7 +12,7 @@ public interface SocketManager {
     void delSocket(String destIP, int destPort);
 
     // Sending the message to the corresponding server
-    void sendMessage(String destIP, int destPost, String payload);
+    void sendMessage(String destIP, int destPort, String payload);
 
     // Checking whether the message is in the queue
     boolean isMessage();
