71d9eec8f4350a68ebd7aca9581d44fb48e056b1
Seungsu Shin <dumble140@gmail.com>
2017-02-13 16:15:40 +0900

    added VPNTest

---
+++ VPNTest/app/src/main/java/org/locationprivacy/vpntest/IP_Packet.java
@@ -0,0 +1,15 @@
+package org.locationprivacy.vpntest;
+
+/**
+ * Created by 신승수 on 2016-10-25.
+ */
+public class IP_Packet {
+    private byte[] packet;
+    private int ihl;
+    IP_Packet(byte[] input_packet){
+        packet = input_packet;
+    }
+    int get_Ihl(){
+        return ((int)(packet[0] & 0x0f)) * 4;
+    }
+}
