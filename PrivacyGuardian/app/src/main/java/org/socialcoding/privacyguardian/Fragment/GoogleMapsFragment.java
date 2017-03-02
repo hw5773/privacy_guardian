bef3a0ac7d26fb0b3175b9ac221de7f7167b8e5a
Seungsu Shin <dumble140@gmail.com>
2017-03-02 16:24:40 +0900

    making google maps

--- PrivacyGuardian/app/src/main/java/org/socialcoding/privacyguardian/Fragment/GoogleMapsFragment.java
+++ PrivacyGuardian/app/src/main/java/org/socialcoding/privacyguardian/Fragment/GoogleMapsFragment.java
@@ -1,6 +1,9 @@
 package org.socialcoding.privacyguardian.Fragment;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,10 +11,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.socialcoding.privacyguardian.Activity.MainActivity;
 import org.socialcoding.privacyguardian.R;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
+ *
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
  * {@link GoogleMapsFragment.OnFragmentInteractionListener} interface
@@ -19,92 +34,296 @@ import org.socialcoding.privacyguardian.R;
  * Use the {@link GoogleMapsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class GoogleMapsFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class GoogleMapsFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMarkerDragListener,
+        GoogleMap.OnMapLoadedCallback, GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnCameraIdleListener {
+    final String baseUrl = "http://cctvs.nineqs.com";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MainActivity mainActivity;
+    private GoogleMap mMap;
+    protected View view;
+    private Location currLocation;
+    private Bitmap blueMarkerIcon;
 
-    private OnFragmentInteractionListener mListener;
-
-    public GoogleMapsFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment GoogleMapsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static GoogleMapsFragment newInstance(String param1, String param2) {
-        GoogleMapsFragment fragment = new GoogleMapsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private static Marker reportMarker;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        int iconSize = 90;
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.blue_marker);
+        blueMarkerIcon = Bitmap.createScaledBitmap(bitmap, iconSize, iconSize, false);
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_google_maps, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        if (view != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            if (parent != null)
+                parent.removeView(view);
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            view = inflater.inflate(R.layout.fragment_google_maps, container, false);
+        }
+
+        if(mMap == null) {
+            SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+        }
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        if (!(getActivity() instanceof MainActivity)) {
+            throw new RuntimeException("Invalid activity.");
+        }
+        mainActivity = (MainActivity) getActivity();
+
+        /* Only works in real phone
+        if(mMap == null) {
+            SupportMapFragment fragment = SupportMapFragment.newInstance();
+            FragmentTransaction fragmentTransaction = getChildFragmentManager().beginTransaction();
+            fragmentTransaction.add(R.id.map, fragment);
+            fragmentTransaction.commit();
+            fragment.getMapAsync(this);
+        }
+        */
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMarkerDragListener(this);
+        mMap.getUiSettings().setCompassEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMapToolbarEnabled(false);
+        // Set zoom level
+        mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
+
+        // Add a marker in Seoul, Korea and move the camera
+        LatLng cityHallSeoul = new LatLng(37.5667151,126.9781312);
+        /*
+        mMap.addMarker(new MarkerOptions().position(cityHallSeoul)
+                .draggable(true).title("Marker in Seoul")); */
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(cityHallSeoul));
+
+        if(ContextCompat.checkSelfPermission(getActivity(),
+                EyeOfSeoulPermissions.LOCATION_PERMISSION_STRING) == EyeOfSeoulPermissions.GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        }
+
+        mMap.setOnMapLoadedCallback(this);
+        mMap.setOnCameraIdleListener(this);
+        mMap.setOnMarkerClickListener(this);
+
+        markers = new Markers();
+
+        mMap.setInfoWindowAdapter(new CctvInfoWindowAdapter(mainActivity));
+    }
+
+    @Override
+    public void onCameraIdle() {
+        if (markers != null) {
+            for (Marker m : markers) {
+                //m.remove();
+            }
+        }
+        CameraPosition cameraPosition = mMap.getCameraPosition();
+        getCctvs(cameraPosition.zoom, cameraPosition);
+    }
+
+    @Override
+    public void onMapLoaded() {
+    }
+
+    private void getCctvs(float zoom, CameraPosition cameraPosition) {
+        // Add initial makers.
+        VisibleRegion visibleRegion = mMap.getProjection().getVisibleRegion();
+        LatLngBounds latLngBounds = visibleRegion.latLngBounds;
+        LatLng northeast = latLngBounds.northeast;
+        LatLng southwest = latLngBounds.southwest;
+        double east = northeast.longitude, north = northeast.latitude;
+        double south = southwest.latitude, west = southwest.longitude;
+
+        try {
+            if (zoom > 14) {
+                IServerResource serverResource = new CCTVHttpHandlerV1(baseUrl);
+                serverResource.getCCTVLocationsAsync(east, north, south, west,
+                        new IRESTAsyncServiceHandler.ICCTVLocationResponse() {
+                            @Override
+                            public void onSuccess(List<CCTVLocationData> cctvLocationDatas) {
+                                for (CCTVLocationData cctv : cctvLocationDatas) {
+                                    if(!markers.contains(cctv.getCctvId())){
+                                        Marker m = markers.getMarkerByLatLng(new LatLng(cctv.getLatitude(), cctv.getLongitude()));
+                                        if (m == null) {
+                                            m = mMap.addMarker(new MarkerOptions().position(
+                                                    new LatLng(cctv.getLatitude(), cctv.getLongitude())));
+                                            m.setDraggable(true);
+                                            m.setIcon(BitmapDescriptorFactory.fromBitmap(blueMarkerIcon));
+                                        }
+                                        markers.add(m, cctv.getCctvId());
+                                        if ("PRIVATE".equals(cctv.getSource())) {
+                                            //m.se;
+                                        }
+                                    }
+
+                                }
+                            }
+
+                            @Override
+                            public void onError() {
+                                System.out.println("ERROR] Async getCCTVLocation Test");
+                            }
+                        });
+            }/* else {
+                Marker numOfMarkers = mMap.addMarker(new MarkerOptions().position(
+                        new LatLng((south + north) / 2, (east + west) / 2)));
+                markers.add(numOfMarkers, -1);
+            }*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+        Toast.makeText(mainActivity.getApplicationContext(), markers.getCctvIdsByMarker(marker).toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+        Double latitude = marker.getPosition().latitude;
+        Double longitude = marker.getPosition().longitude;
+        if(reportMarker != null) {
+            currLocation = new Location("dummy");
+            currLocation.setLatitude(latitude);
+            currLocation.setLongitude(longitude);
+            reportMarker.setTitle("위도 : " + Double.toString(latitude) + ", " +
+                    "경도 : " + Double.toString(longitude));
+            // reportMarker.setSnippet(getAddress()); // Too slow...
+            marker.showInfoWindow();
         }
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onMarkerDragEnd(Marker marker) {
+        Double latitude = marker.getPosition().latitude;
+        Double longitude = marker.getPosition().longitude;
+        if(reportMarker != null) {
+            reportMarker.setTitle("위도 : " + Double.toString(latitude) + ", " +
+                    "경도 : " + Double.toString(longitude));
+            reportMarker.setSnippet(getAddress());
+            marker.showInfoWindow();
+        }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void moveCamera(LatLng latLng) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    public boolean moveCurrentPosition() {
+        if(ContextCompat.checkSelfPermission(getActivity(),
+                EyeOfSeoulPermissions.LOCATION_PERMISSION_STRING) == EyeOfSeoulPermissions.GRANTED) {
+            if(MainActivity.client != null) {
+                currLocation = LocationServices.FusedLocationApi
+                        .getLastLocation(MainActivity.client);
+                if (currLocation != null) {
+                    LatLng currLatLng = new LatLng(currLocation.getLatitude(),
+                            currLocation.getLongitude());
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(currLatLng));
+                    getActivity().findViewById(R.id.bottom_bar_google_map_loading_text_view)
+                            .setVisibility(View.INVISIBLE);
+                    getActivity().findViewById(R.id.bottom_bar_google_map_ask_layout)
+                            .setVisibility(View.VISIBLE);
+                    if (reportMarker != null) {
+                        reportMarker.remove();
+                    }
+                    reportMarker = mMap.addMarker(new MarkerOptions().position(currLatLng)
+                            .draggable(true).visible(true)
+                            .title("위도 : " + currLocation.getLatitude() + ", " +
+                                    "경도 : " + currLocation.getLongitude())
+                            .snippet(getAddress()));
+                }
+            }
+        }
+        return true;
+    }
+
+    public void removeMarker() {
+        if(reportMarker != null) {
+            reportMarker.remove();
+        }
+    }
+
+    public String getAddress() {
+        double lat = currLocation.getLatitude();
+        double lng = currLocation.getLongitude();
+        String addr = null;
+        Geocoder geocoder = new Geocoder(getActivity(), Locale.KOREA);
+        List<Address> address;
+        try {
+            address = geocoder.getFromLocation(lat, lng, 1);
+            if (address != null && address.size() > 0) {
+                addr = address.get(0).getAddressLine(0);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        MainActivity.address = addr;
+        return addr;
+    }
+
+    public void onSearchButtonClick(String searchText) {
+        new Thread(new SearchHttpHandler(searchText)).start();
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        marker.showInfoWindow();
+        return true;
+    }
+
+
+    public class SearchHttpHandler implements Runnable {
+        String searchText;
+
+        SearchHttpHandler(String searchText) {
+            this.searchText = searchText;
+        }
+
+        public void run() {
+            OkHttpClient client = new OkHttpClient();
+            Request request = new Request.Builder()
+                    .url("https://maps.googleapis.com/maps/api/place/textsearch/" +
+                            "json" +
+                            "?query=" + searchText +
+                            "&key=" + getResources().getString(R.string.webservice_key))
+                    .build();
+
+            try {
+                Response response = client.newCall(request).execute();
+                String result = response.body().string();
+                System.out.println(result);
+                JSONObject jsonObject = new JSONObject(result);
+                Double lat = (Double) jsonObject.getJSONArray("results").getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location").get("lat");
+                Double lng = (Double) jsonObject.getJSONArray("results").getJSONObject(0)
+                        .getJSONObject("geometry").getJSONObject("location").get("lng");
+                final LatLng latLng = new LatLng(lat, lng);
+
+                mainActivity.runOnUiThread(new Runnable() {
+                    public void run() {
+                        ((GoogleMapFragment) mainActivity.googleMapFragment).moveCamera(latLng);
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
