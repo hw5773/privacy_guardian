2dd638188966486b9d58511085d0a2fe8ace132a
Seungsu Shin <dumble140@gmail.com>
2017-03-05 13:25:48 +0900

    mapfragment using view

--- PrivacyGuardian/app/src/main/java/org/socialcoding/privacyguardian/Fragment/GoogleMapsFragment.java
+++ PrivacyGuardian/app/src/main/java/org/socialcoding/privacyguardian/Fragment/GoogleMapsFragment.java
@@ -16,11 +16,13 @@ import org.socialcoding.privacyguardian.R;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -29,16 +31,55 @@ import static org.socialcoding.privacyguardian.R.id.container;
 import static org.socialcoding.privacyguardian.R.id.map;
 import static org.socialcoding.privacyguardian.R.id.submenuarrow;
 
-public class GoogleMapsFragment extends Fragment {
+public class GoogleMapsFragment extends Fragment implements OnMapReadyCallback{
 
     private GoogleMap googleMap;
     private MapView mapView;
 
     @Override
+    public void onMapReady(GoogleMap map){
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0,0))
+                .title("Marker"));
+    }
+    /*
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        mapView.onCreate(savedInstanceState);
+    }*/
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_google_maps, container, false);
+
         mapView = (MapView) rootView.findViewById(R.id.map);
         mapView.onCreate(savedInstanceState);
+
+        mapView.onResume();  // needed to get the map to display immediately
+        try{
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        //from http://stackoverflow.com/questions/19353255/how-to-put-google-maps-v2-on-a-fragment-using-viewpager
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                googleMap = mMap;
+
+                //For showing a move to my location button
+               // googleMap.setMyLocationEnabled(true);
+
+                //For dropping a marker at a point on the Map
+                LatLng sydney = new LatLng(-34,151);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker Title").snippet("Marker Description"));
+
+                //For zooming automatically to the location of the marker
+                CameraPosition cameraPosition = new CameraPosition.Builder().target(sydney).zoom(12).build();
+                googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            }
+        });
 
         return rootView;
     }
