dd71497aaa7037a9ceb80cf64f6b0a7ab68455e0
Kim Doyoung <nhne@users.noreply.github.com>
2017-10-13 15:23:23 +0900

    initial credential manager

---
+++ PrivacyGuardian/app/src/main/java/org/socialcoding/privacyguardian/Credential/CredentialManager.java
@@ -0,0 +1,21 @@
+package org.socialcoding.privacyguardian.Credential;
+
+/**
+ * Created by disxc on 2017-10-13.
+ */
+
+public class CredentialManager {
+    //TODO: CREATE appropriate interface for Credential manager.
+    static boolean checkRootCA() {
+        return false;
+    }
+
+    static boolean installRootCA() {
+        return false;
+    }
+
+    static boolean removeRootCA() {
+        return false;
+    }
+
+}
